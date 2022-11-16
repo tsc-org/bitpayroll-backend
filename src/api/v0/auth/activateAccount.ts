bb71ee72fa847036bf57fb5ddc2e65f626b1cbe2
Samson Olusegun <olusegunsamson@live.com>
2022-11-16 15:41:20 +0100

    fix: email transporter

--- src/api/v0/auth/activateAccount.ts
+++ src/api/v0/auth/activateAccount.ts
@@ -1,33 +1,38 @@
-import {Router, Request, Response} from 'express';
-import {PrismaClient} from '@prisma/client';
+import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router: Router = Router();
-
+const prisma = new PrismaClient();
 
 //route for activating account
-router.put('/activate',  async (req:Request, res:Response, next) => {
-    const prisma = new PrismaClient();
+router.put("/activate", async (req: Request, res: Response, next) => {
+  try {
     const token = req.params.token;
     const user = await prisma.user.findFirst({
-        where: {
-          secretToken: token
-        }
+      where: {
+        secretToken: token,
+      },
     });
-    if(!user) {
-        res.status(400).json({message: "Invalid token"});
+    if (!token) {
+      res
+        .status(400)
+        .json({ message: "Invalid token or account activated" });
     } else {
-        await prisma.user.update({
-            where: {
-                id: user.id
-            },
-            data: {
-                isActive: true,
-                secretToken: null
-            }
-        });
-        res.status(200).json({message: "Account activated"});
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          isActive: true,
+          secretToken: null,
+        },
+      });
+      res.status(200).json({ message: "Account activated" });
     }
-}
-);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    throw new Error(error);
+  }
+});
 
 export const ConfirmRouter: Router = router;
