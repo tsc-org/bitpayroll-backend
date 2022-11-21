15ba427b90d9b4b1ba6eb4347acb9857900cd48c
Samson Olusegun <olusegunsamson@live.com>
2022-11-22 00:27:59 +0100

    feat: employee list

--- src/api/v0/organisation/org.router.ts
+++ src/api/v0/organisation/org.router.ts
@@ -3,6 +3,7 @@ import { UpdateAccountRouter } from "./routes/profile";
 import { WalletRouter } from "./routes/createWallet";
 import { InviteEmployeeRouter } from "./routes/inviteEmployee";
 import { SendBitcoinRouter } from "./routes/sendBitcoin";
+import { ListEmployeeRouter } from "./routes/listEmployee";
 
 const router: Router = Router();
 
@@ -15,6 +16,8 @@ router.use("/org", UpdateAccountRouter);
 router.use("/org", WalletRouter);
 router.use("/org", SendBitcoinRouter);
 router.use("/org", InviteEmployeeRouter);
+router.use("/org", ListEmployeeRouter);
+
 
 
 
